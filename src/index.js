7161af56f611fd1471659304344ee528a4eeffe7
Marc Mauger <simianarmy@gmail.com>
2021-10-28 15:41:45 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
+import { getTodo, listTodos } from './graphql/queries';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+const { program } = require('commander');
+const inquirer = require('inquirer');
+
+(async () => {
+    console.log("Demo app");
+
+    async function getTodoByName() {
+        const answer = await inquirer.prompt([
+            {name: 'title', message: 'Enter a title: '},
+        ]);
+        const searchTerm = answer.title;
+        // fetch list and search by title
+        try {
+            const todos = await API.graphql(graphqlOperation(listTodos, {filter: {name: {eq: searchTerm}}}));
+            const result = todos.data.listTodos.items[0];
+
+            if (!result) {
+                console.warn(`No Todo named ${searchTerm}`);
+            }
+            return result;
+        } catch (err) {
+            console.log("filter operation failed", err);
+        }
+        return null;
+    }
+
+    program
+        .description('Interact with our Todo GraphQL API')
+        .option('-l, --list', 'list all todos')
+        .option('-n, --new', 'new todo')
+        .option('-u, --update', 'update todo')
+        .option('-d, --delete', 'delete todo');
+
+        program.parse();
+        const options = program.opts();
+
+        if (options.list) {
+            console.log("listing todos:");
+            const todos = await API.graphql(graphqlOperation(listTodos));
+            console.log(todos.data.listTodos);
+        } else if (options.new) {
+            /* create a todo */
+            const answers = await inquirer.prompt(
+                [
+                    {name: 'title', message: 'Enter a title: '},
+                    {name: 'description', message: 'Enter a description: '}
+                ])
+            if (answers.title !== '' && answers.description !== '') {
+                const todo = { name: answers.title, description: answers.description };
+                await API.graphql(graphqlOperation(createTodo, {input: todo}));
+                console.log("Added Todo", todo);
+            } else {
+                console.log("Invalid input");
+            }
+        } else if (options.update) {
+            const todo = await getTodoByName();
+            if (todo) {
+                console.log("updating todo");
+                try {
+                    await API.graphql(graphqlOperation(updateTodo, { input: { id: todo.id, description: 'updated todo desc' } } ));
+                } catch (err) {
+                    console.warn("GraphQL error", err);
+                }
+            }
+        } else if (options.delete) {
+            const todo = await getTodoByName();
+            if (todo) {
+                console.log("deleting todo");
+                try {
+                    await API.graphql(graphqlOperation(deleteTodo, { input: { id: todo.id } }));
+                } catch (err) {
+                    console.warn("GraphQL error", err);
+                }
+            }
+        } else {
+            program.showHelpAfterError();
+        }
+})();
